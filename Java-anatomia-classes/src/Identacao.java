5f1ea45b126f59aa442407204d1a75f9a1c92571
Ginizu <diniztorres.giovanna@gmail.com>
2024-09-25 20:30:40 -0300

    Identação

---
+++ Java-anatomia-classes/src/Identacao.java
@@ -0,0 +1,10 @@
+public class Identacao {
+    public static void main(String[] args) {
+        int media = 6;
+        if (media<=6)
+            System.out.println("Reprovado");
+        else
+            System.out.println("Aprovado"); 
+        // Aqui podemos analisar que um código bem identado pode nos ajudar uma melhor leitura e melhor prática de desenvolvimento, não altera em questões de resultado.
+    }
+}
